34af0b404ed7508d138f531221ef716ccc29fd3b
Sarabeshwaran <113020922+Sarabeshwaran@users.noreply.github.com>
2023-05-05 08:42:34 +0530

    Create Barplot.r

---
+++ Barplot.r
@@ -0,0 +1,6 @@
+CODE :
+
+vehicles <- c("Bike", "Bus", "Car", "Auto", "Train")
+counts <- c(10 20, 30, 40, 50)
+barplot(names.arg = vehicles, counts,
+      main = "Vehicle Graph", xlab = "Vehicle", ylab = "Counts")
