aaf0d85fc260e7018e40c50cc2fd5989a62106ad
Sarabeshwaran <113020922+Sarabeshwaran@users.noreply.github.com>
2023-05-05 08:47:32 +0530

    Create Division.r

---
+++ Division.r
@@ -0,0 +1,14 @@
+CODE :
+
+number_One <- as.integer(readline("Enter the first number  : "))
+number_Two <- as.integer(readline("Enter the second number : "))
+print(paste("Division of",number_One, "and",number_Two, "is :",number_One / number_Two))
+
+OUTPUT :
+
+> number_One <- as.integer(readline("Enter the first number  : "))
+Enter the first number  : 9
+> number_Two <- as.integer(readline("Enter the second number : "))
+Enter the second number : 03
+> print(paste("Division of",number_One, "and",number_Two, "is :",number_One / number_Two))
+[1] "Division of 9 and 3 is : 3"
