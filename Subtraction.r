7fad82fed7f008727a2194d724b30716b6de71f7
Sarabeshwaran <113020922+Sarabeshwaran@users.noreply.github.com>
2023-05-05 08:57:46 +0530

    Create Subtraction.r

---
+++ Subtraction.r
@@ -0,0 +1,14 @@
+CODE :
+
+number_One <- as.integer(readline("Enter the first number : "))
+number_Two <- as.integer(readline("Enter the second number : "))
+print(paste("Subtraction of",number_One, "and",number_Two, "is :",number_One - number_Two))
+
+OUTPUT : 
+
+> number_One <- as.integer(readline("Enter the first number : "))
+Enter the first number : 8
+> number_Two <- as.integer(readline("Enter the second number : "))
+Enter the second number : 5
+> print(paste("Subtraction of",number_One, "and",number_Two, "is :",number_One - number_Two))
+[1] "Subtraction of 8 and 5 is : 3"
