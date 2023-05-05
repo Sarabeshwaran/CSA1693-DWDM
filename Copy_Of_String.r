642509b9473c0a1f2ba0be5beabcee70febf6afd
Sarabeshwaran <113020922+Sarabeshwaran@users.noreply.github.com>
2023-05-05 08:44:11 +0530

    Create Copy_Of_String.r

---
+++ Copy_Of_String.r
@@ -0,0 +1,15 @@
+CODE :
+
+user_input <- readline("Enter a string : ")
+print(paste("Original_String : ",user_input)) 
+copied_String <- user_input
+print(paste("Copied_String:", copied_String))
+
+OUTPUT :
+
+Enter a string : Hey
+> print(paste("Original_String : ",user_input)) 
+[1] "Original_String :  Hey"
+> copied_String <- user_input
+> print(paste("Copied_String:", copied_String))
+[1] "Copied_String: Hey"
