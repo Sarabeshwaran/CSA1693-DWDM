1d049c86587cfdcc956535c17c222e634143fa24
Sarabeshwaran <113020922+Sarabeshwaran@users.noreply.github.com>
2023-05-05 08:58:41 +0530

    Create Variance.r

---
+++ Variance.r
@@ -0,0 +1,12 @@
+CODE:
+
+data <- c(10, 20, 30, 40, 50)
+variance <- var(data)
+print(paste("Variance:", variance))
+
+OUTPUT:
+
+> data <- c(10, 20, 30, 40, 50)
+> variance <- var(data)
+> print(paste("Variance:", variance))
+[1] "Variance: 250"
