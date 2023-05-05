1dcb4306471307894e0b94a1697a4dc3ced42560
Sarabeshwaran <113020922+Sarabeshwaran@users.noreply.github.com>
2023-05-05 08:40:19 +0530

    Create Addition.r

---
+++ Addition.r
@@ -0,0 +1,14 @@
+CODE :
+
+number_One <- as.integer(readline("Enter the first number  : "))
+number_Two <- as.integer(readline("Enter the second number : "))
+print(paste("Addition of",number_One, "and",number_Two, "is :",number_One + number_Two))
+
+OUTPUT :
+
+> number_One <- as.integer(readline("Enter the first number  : "))
+Enter the first number  : 18
+> number_Two <- as.integer(readline("Enter the second number : "))
+Enter the second number : 02
+> print(paste("Addition of",number_One, "and",number_Two, "is :",number_One + number_Two))
+[1] "Addition of 18 and 2 is : 20"
