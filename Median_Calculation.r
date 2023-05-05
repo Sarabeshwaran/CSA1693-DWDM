189c258763b7249fd07512686311c638a880755a
Sarabeshwaran <113020922+Sarabeshwaran@users.noreply.github.com>
2023-05-05 08:51:39 +0530

    Create Median_Calculation.r

---
+++ Median_Calculation.r
@@ -0,0 +1,12 @@
+CODE:
+
+data <- c(10, 20, 30, 40, 50, 50, 60, 70, 80, 90, 90)
+median_value <- median(data)
+print(paste("Median   : ", median_value))
+
+OUTPUT:
+
+> data <- c(10, 20, 30, 40, 50, 50, 60, 70, 80, 90, 90)
+> median_value <- median(data)
+> print(paste("Median   : ", median_value))
+[1] "Median   :  50"
