63713fe4695050f030fb0b326abefac699d79b28
Sarabeshwaran <113020922+Sarabeshwaran@users.noreply.github.com>
2023-05-05 08:49:33 +0530

    Create Mean_Calculation.r

---
+++ Mean_Calculation.r
@@ -0,0 +1,12 @@
+CODE :
+
+data <- c(10, 20, 30, 40, 50, 50, 60, 70, 80, 90, 90)
+mean_value <- mean(data)
+print(paste("Mean   : ", mean_value))
+
+OUTPUT :
+
+> data <- c(10, 20, 30, 40, 50, 50, 60, 70, 80, 90, 90)
+> mean_value <- mean(data)
+> print(paste("Mean   : ", mean_value))
+[1] "Mean   :  53.6363636363636"
